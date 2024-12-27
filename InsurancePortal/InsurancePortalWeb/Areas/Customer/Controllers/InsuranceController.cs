9f084e6a8d41e0e0a7b12f4a1c4c04e146c4f925
Kristaps Kozlovskis <160219564+codekristaps@users.noreply.github.com>
2024-12-27 21:59:23 +0200

    Removed the users to simplify the prroject

---
+++ InsurancePortal/InsurancePortalWeb/Areas/Customer/Controllers/InsuranceController.cs
@@ -0,0 +1,23 @@
+﻿using InsurancePortal.DataAccess;
+using InsurancePortal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsurancePortalWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class InsuranceController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public InsuranceController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Insurance> objCategoryList = _db.Insurances.ToList();
+
+            return View(objCategoryList);
+        }
+    }
+}
